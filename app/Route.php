697754f23c8be1e174d8a3a058a3580abcb6cb4a
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-05-13 21:52:32 +1000

    .

---
+++ app/Route.php
@@ -0,0 +1,9 @@
+<?php
+
+class Route {
+	public static $routes = require '../config/routes.php';
+	protected static $params = [];
+	public static add() {
+		//
+	}
+}
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once '../app/Route.php';
+echo Route::routes;
+?>

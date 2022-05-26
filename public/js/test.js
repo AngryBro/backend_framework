f10f78c7a99f4fea187706b136c177d8a0f69af5
AngryBro <67253990+AngryBro@users.noreply.github.com>
2022-05-26 21:48:20 +1000

    Update test.js

--- public/js/test.js
+++ public/js/test.js
@@ -1,4 +1,9 @@
 var answers = {};
+var json_div = document.getElementById('json');
+var json = json_div.innerHTML;
+json = JSON.parse(json);
+var files = json['files'];
+
 function disable_button(button) {
     //button.setAttribute('disabled',true);
     button.setAttribute('class','disabled');
@@ -13,23 +18,29 @@ function save(task,ans,button) {
 function del(ans) {
     ans.value = '';
 }
+function make_active(button) {
+    for(var i in files) {
+        if(this.id=='button'+i) {
+            document.getElementById('button'+i).setAttribute('class','active_page');
+        }
+        else {
+            document.getElementById('button'+i).setAttribute('class','page');
+        }
+    }
+}
 function create_button(img,text) {
     return `
     <tr>
         <td>
-            <button class='page' onclick='img.src="/img/`+img+`"'>`+text+`
+            <button class='page' id=button`+text+` onclick='make_active(this);img.src="/img/`+img+`"'>`+text+`
             </button>
         </td>
     </tr>
     `;
 }
 function build() {
-    var json_div = document.getElementById('json');
-    var json = json_div.innerHTML;
-    json = JSON.parse(json);
     //json_div.remove();
     var buttons = document.getElementById('buttons');
-    var files = json['files'];
     buttons.innerHTML += create_button(files['i'],'<i>i</i>');
     for(var i in files) {
         if(i!='i') {
